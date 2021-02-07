de032c1c44c661ad6ec04b93d290b7f171db4977
ZzoYo <36184794+ZzoYo@users.noreply.github.com>
2021-02-07 10:54:08 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,7 +20,7 @@
 # Add liuran package
 #sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default
 
-# Add liuran package
+# Add kenzok8 package
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
